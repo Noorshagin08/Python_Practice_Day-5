269228167764716b5f6efb85a50bb3bcc43f9436
Noorshagin08 <noorshagin05@gmail.com>
2025-06-25 18:41:24 +0530

    class and object

---
+++ class and object.py
@@ -0,0 +1,70 @@
+
+#create one class(goa) for two object(ramesh,suresh)
+
+# class goa:
+#     name=""
+#     drink=""
+#     def party(self):
+#         print("lets party...")
+#     def beach(self):
+#         print("enjoy the beach...")
+
+# ramesh=goa() 
+# suresh=goa() 
+
+# ramesh.name="Ramesh"
+# suresh.name="suresh"
+
+# ramesh.drink="yes"
+# suresh.drink="no"
+
+# print(ramesh.name)
+# print("drink:",ramesh.drink)
+
+# print(suresh.name)
+# print("drink:",suresh.drink)
+
+# ramesh.party()
+# suresh.beach()
+
+# 1) create a class called laptop and create a following variable and function
+
+        #variables=price,processor,ram
+        # object HP,DELL,LENOVO
+
+class laptop:
+    price=0
+    processor=""
+    ram=""
+
+HP =laptop()
+DELL=laptop()
+LENOVA=laptop()
+
+HP.price="40000" 
+HP.processor="intel"
+HP.ram="64 gb"
+
+DELL.price="50000" 
+DELL.processor="AMD"
+DELL.ram="64 gb"
+
+LENOVA.price="80000" 
+LENOVA.processor="intel"
+LENOVA.ram="16 gb"
+
+print(HP.price) 
+print(HP.processor)
+print(HP.ram)
+
+print(DELL.price)
+print(DELL.processor)
+print(DELL.ram)
+
+print(LENOVA.price) 
+print(LENOVA.processor)
+print(LENOVA.ram)
+
+
+
+
